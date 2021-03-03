e749a102bc1e9c3d3cace4d1ac50b5ed4d416812
unknown <alia.azarian@gmail.com>
2021-03-03 18:56:04 +0000

    maps page added

--- src/pages/pickupgames.js
+++ src/pages/pickupgames.js
@@ -29,39 +29,29 @@ const PickUpGames = (props) => (
                         <h1>PickUp Games</h1>
                     </header>
 
-                    <h4 style={{textAlign: "justify", fontWeight:"bold"}}>A social networking app made for sports enthusiasts who 
-                    want like-minded users to play against<br />
-                    Users can create game posts, edit and filter them, join games, upload photos and chat with other users
-                    <br />Made with .Net Core on the backend, react on the frontend with CRUD functionality for communicating between the two
-                    <br /><br />
-                    <i style={{color:'yellow'}}>.Net Core - react - typescript - MobX - Entity Framework - SqLite/MySql/SqlServer - Semantic-UI</i><br />
+                    <h4 style={{textAlign: "justify", fontWeight:"bold"}}>
+                        A social networking app made for sports enthusiasts who want like-minded users to play against
+                        <br /><br />Users can create game posts, edit and filter them, join games, upload photos and chat with other users
+                        <br /><br />Made with .Net Core on the backend, react on the frontend with CRUD functionality for communicating between the two                        
                     </h4>
 
-                    <br />
-
-                    <h5><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/35px-.NET_Core_Logo.svg.png"/><i>&nbsp;&nbsp;.NET:&nbsp; A framework for building a variety of apps that's liked for it's speed, versatility and scalability</i></h5>
-                    <h5><img src="https://img.icons8.com/plasticine/45/000000/react.png"/><i>&nbsp;&nbsp;React:&nbsp; A language that lets you strongly-type Javascript so you can keep track of your data-types and use the latest features of Javascript</i></h5>
-                    <h5><img src="https://img.icons8.com/color/40/000000/typescript.png"/><i>&nbsp;&nbsp;Typescript:&nbsp; For users to manually sign-in and be authenticated/stored on the Firebase database</i></h5>
-                    <h5><img src="https://api.iconify.design/logos-mobx.svg?height=30"/><i>&nbsp;&nbsp;&nbsp;&nbsp;MobX:&nbsp; A library that helps simplify components by moving state from local to central - make components 'observers' of state in stores using react 'context'</i></h5>
-                    <h5><img src="https://img.icons8.com/nolan/40/data-configuration.png"/><i>&nbsp;&nbsp;&nbsp;Entity Framework:&nbsp; An Object-Relational Mapper that lets you communicate with a SQL database without having to use SQL queries</i></h5>                    
-                    <h5><img src="https://img.icons8.com/nolan/45/sql.png"/><i>&nbsp;&nbsp;&nbsp;SQL:&nbsp; Used SqLite in development for seeding data and swapped it out for MySql and SqlServer at deployment</i></h5>
-                    <h5><img src="https://api.iconify.design/logos-semantic-ui.svg?height=35"/><i>&nbsp;&nbsp;&nbsp;Semantic-UI:&nbsp; An easy to use style/theme framework with ready-to-use components and makes responsive layout easier to build</i></h5>
+                    <hr />                    
+                    <h4 style={{textAlign: "center", color:'yellow'}}><i>.Net Core - react - typescript - MobX - Entity Framework - SqLite/MySql/SqlServer - Semantic-UI</i></h4>                                     
+                    <hr />                    
                     
-                    <br />
-                    
-                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Overview of app</p>
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}> Logging-in and out as a previously registered user with client-side form validation</p>
                     <span className="image main"><img src={log_in_bob} alt="" /></span>
 
-                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Checking-out with payment and address details via the Stripe API</p>
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Alternatively, facebook users can log-in via the second button (facebook issues an access token which this app verifies by sending it back to facebook and then generates a JWT token to the facebook user)</p>
                     <span className="image main"><img src={facebook_login} alt="" /></span>
 
-                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Manually signing-in as an existing user and having user data requested from the Firebase database</p>
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>A logged-in user can return to the site without logging-in again because persistence has been added by sending data to the server.  Once in, a user can create a game - this form also has client-side validation</p>
                     <span className="image main"><img src={login_persistence_create_game} alt="" /></span>
 
-                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Signing-in and being authenticated as a Google user</p>
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Only the creator of a game can then edit it - the Manage button only appears if the user is also the creator</p>
                     <span className="image main"><img src={Edit_game_add_comment} alt="" /></span>
 
-                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Signing-in and being authenticated as a Google user</p>
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>A user can look up a game and join - multiple users can add messages in the chat box under every game (SignalR)</p>
                     <span className="image main"><img src={chat_widget} alt="" /></span>
 
                     <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Signing-in and being authenticated as a Google user</p>
@@ -72,6 +62,17 @@ const PickUpGames = (props) => (
 
                     <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Signing-in and being authenticated as a Google user</p>
                     <span className="image main"><img src={add_photos} alt="" /></span>
+
+                    <br />                    
+
+                    <h5><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/35px-.NET_Core_Logo.svg.png"/><i>&nbsp;&nbsp;.NET:&nbsp; A framework for building a variety of apps that's liked for it's speed, versatility and scalability</i></h5>
+                    <h5><img src="https://img.icons8.com/plasticine/45/000000/react.png"/><i>&nbsp;&nbsp;React:&nbsp; A language that lets you strongly-type Javascript so you can keep track of your data-types and use the latest features of Javascript</i></h5>
+                    <h5><img src="https://img.icons8.com/color/40/000000/typescript.png"/><i>&nbsp;&nbsp;Typescript:&nbsp; For users to manually sign-in and be authenticated/stored on the Firebase database</i></h5>
+                    <h5><img src="https://api.iconify.design/logos-mobx.svg?height=30"/><i>&nbsp;&nbsp;&nbsp;&nbsp;MobX:&nbsp; A library that helps simplify components by moving state from local to central - make components 'observers' of state in stores using react 'context'</i></h5>
+                    <h5><img src="https://img.icons8.com/nolan/40/data-configuration.png"/><i>&nbsp;&nbsp;&nbsp;Entity Framework:&nbsp; An Object-Relational Mapper that lets you communicate with a SQL database without having to use SQL queries</i></h5>                    
+                    <h5><img src="https://img.icons8.com/nolan/45/sql.png"/><i>&nbsp;&nbsp;&nbsp;SQL:&nbsp; Used SqLite in development for seeding data and swapped it out for MySql and SqlServer at deployment</i></h5>
+                    <h5><img src="https://api.iconify.design/logos-semantic-ui.svg?height=35"/><i>&nbsp;&nbsp;&nbsp;Semantic-UI:&nbsp; An easy to use style/theme framework with ready-to-use components and makes responsive layout easier to build</i></h5>
+                    
                 </div>
 
             </section>
--- src/pages/realfinx-maps.js
+++ src/pages/realfinx-maps.js
@@ -2,29 +2,50 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import pic11 from '../assets/images/pic11.jpg'
+import various_charts from '../assets/images/various_charts.gif'
+import investors_world_map from '../assets/images/investors_world_map.gif'
+import investors_barchart from '../assets/images/investors_barchart.gif'
+
 
 const RealFinXMaps = (props) => (
     <Layout>
         <Helmet>
-            <title>RealFinX Maps - Forty by HTML5 UP</title>
-            <meta name="description" content="RealFinXMaps Page" />
+            <title>RealFinX Maps</title>
+            <meta name="description" content="LMDRoast Page" />
         </Helmet>
 
         <div id="main" className="alt">
             <section id="one">
+
                 <div className="inner">
                     <header className="major">
-                        <h1>RealFinX Maps</h1>
+                        <h1>RealFinX Maps</h1>                   
                     </header>
-                    <span className="image main"><img src={pic11} alt="" /></span>
-                    <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-                    <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+
+                    <h3 style={{textAlign: "justify", fontWeight:"bold"}}>A <a href="https://aliamk.github.io/Fit-To-Print/" target='blank' style={{textDecoration:"underline"}}>
+                    news app</a> built without any libraries or frameworks to see how much functionality 
+                    can be built in vanilla Javascript, HTML and CSS - Using APIs from The Guardian and the New York Times newspapers
+                    </h3>                          
+                    
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Overview of app</p>
+                    <span className="image main"><img src={investors_world_map} alt="" /></span>
+
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>Can tweet any of the articles from your personal Twitter account</p>
+                    <span className="image main"><img src={investors_barchart} alt="" /></span>
+
+                    <p style={{marginBottom:"-1em", fontWeight:"bold"}}>The NYT API has a 'per-facet' property which lists names of people mentioned in its articles - clicking on them searches both the NYT and the Guardian for that name</p>
+                    <span className="image main"><img src={various_charts} alt="" /></span>
+                    
+                    <ul>
+                        <h4>Post-MVP plans:</h4>
+                        <li>Responsive design - want to make the layout work well on all screen sizes and browsers - currently it looks best in Chrome and sizes +900px</li>
+                        <li>Modular Programming - look at ways to refactor the code to make it more readable, shorter and reusable</li>
+                    </ul>
+                    
                 </div>
+
             </section>
         </div>
-
     </Layout>
 )
 
